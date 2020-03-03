bdfaf2156ffd0466fbec11575a3e97c3c9157d24
Florian <florian.pabich@yahoo.fr>
2020-03-03 15:29:39 +0000

    some examples

--- src/Controller/GeeimapController.php
+++ src/Controller/GeeimapController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class GeeimapController extends AbstractController
 {
-    /**
-     * @Route("/geeimap", name="geeimap")
-     */
     public function index()
     {
-        return $this->render('geeimap/index.html.twig', [
-            'controller_name' => 'GeeimapController',
-        ]);
+        //return $this->render('geeimap/index.html.twig', ['controller_name' => 'GeeimapController',        ]);
+        return new Response('title');
+    }
+
+    public function first_page(){
+    	//return new Response('test');
+    	return $this->render('geeimap/first_page.html.twig');
     }
 }
