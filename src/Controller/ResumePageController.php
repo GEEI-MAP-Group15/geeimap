31a8377109648e37098dea1c32ce9a27d0e1e446
Caroline <caroline.lismonde@sfr.fr>
2020-03-13 11:00:38 +0000

    resume page

---
+++ src/Controller/ResumePageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ResumePageController extends AbstractController
+{
+    /**
+     * @Route("/resume/page", name="resume_page")
+     */
+    public function index()
+    {
+        return $this->render('resume_page/index.html.twig', [
+            'controller_name' => 'ResumePageController',
+        ]);
+    }
+}
