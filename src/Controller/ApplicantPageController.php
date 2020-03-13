93722665ae1ad8e75da1dd777dec6933917e8f1e
Florian <florian.pabich@yahoo.fr>
2020-03-13 12:21:55 +0000

    applicant page id

--- src/Controller/ApplicantPageController.php
+++ src/Controller/ApplicantPageController.php
@@ -4,16 +4,28 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ApplicationRepository;
+use App\Entity\Application;
 
 class ApplicantPageController extends AbstractController
 {
     /**
      * @Route("/applicant/page", name="applicant_page")
      */
-    public function index()
+    public function index(ApplicationRepository $applicationRepository)
     {
+    	$applications = $applicationRepository->findBy([]);
         return $this->render('applicant_page/index.html.twig', [
-            'controller_name' => 'ApplicantPageController',
+            'applications' => $applications,
+        ]);
+    }
+
+
+
+    public function show(Application $application) {
+    	//return new Response('Lire '.$url);
+    	return $this->render('applicant_page/show.html.twig', [
+            'application' => $application
         ]);
     }
 }
