92a282b280d55892486dc42265d540cd62ae38ac
Florian <florian.pabich@yahoo.fr>
2020-03-19 17:56:50 +0100

    routes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/public", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -96,7 +96,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         #throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        return new RedirectResponse($this->urlGenerator->generate('homepage'));
+        return new RedirectResponse($this->urlGenerator->generate('formulaire'));
     }
 
     protected function getLoginUrl()
