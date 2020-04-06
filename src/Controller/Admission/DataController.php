e9a7635bf36ca598956013c01424c1c13b799897
Paul Duval <paulduval@MacBook-Pro-de-Paul.local>
2020-04-06 18:16:19 +0200

    data knowledge controller & template (admission/data)

---
+++ src/Controller/Admission/DataController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller\Admission;
+
+use App\Entity\Application;
+use App\Entity\Degree;
+use App\Entity\AcademicData;
+use App\Entity\AcademicLevel;
+use App\Entity\Background;
+use App\Entity\College;
+use App\Entity\Document;
+use App\Entity\Module;
+use App\Entity\Student;
+use App\Entity\User;
+use App\Repository\ApplicationRepository;
+use App\Repository\DegreeRepository;
+use App\Repository\AcademicDataRepository;
+use App\Repository\AcademicLevelRepository;
+use App\Repository\BackgroundRepository;
+use App\Repository\CollegeRepository;
+//use App\Repository\DocumentRepository;
+use App\Repository\ModuleRepository;
+use App\Repository\StudentRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admission/data")
+ */
+class DataController extends AbstractController
+{
+    /**
+     * @Route("/", name="data_index", methods={"GET"})
+     */
+    public function index(ApplicationRepository $applicationRepository, DegreeRepository $degreeRepository, AcademicDataRepository $academicDataRepository, AcademicLevelRepository $academicLevelRepository, BackgroundRepository $backgroundRepository, CollegeRepository $collegeRepository, /*DocumentRepository $documentRepository,*/ ModuleRepository $moduleRepository, StudentRepository $studentRepository, UserRepository $userRepository): Response
+    {
+        return $this->render('admission/data/index.html.twig', [
+            'applications' => $applicationRepository->findAll(),
+            'degrees' => $degreeRepository->findAll(),
+            'academicdatas' => $academicDataRepository->findAll(),
+            'academiclevels' => $academicLevelRepository->findAll(),
+            'backgounds' => $backgroundRepository->findAll(),
+            'colleges' => $collegeRepository->findAll(),
+            //'documents' => $documentRepository->findAll(),
+            'modules' => $moduleRepository->findAll(),
+            'students' => $studentRepository->findAll(),
+            'users' => $userRepository->findAll(),
+
+        ]);
+    }
+
+    public function getTotalApplicant($applications)
+    {
+    	return var_dump(count($applications));
+    }
+
+}
