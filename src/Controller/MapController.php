bf1b070793c42e08fbddf8b3ce60940396e57125
Florian <florian.pabich@yahoo.fr>
2020-04-06 10:54:30 +0200

    map

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -4,19 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Geocoder\Query\GeocodeQuery;
+use Geocoder\Query\ReverseQuery;
+use App\Repository\BackgroundRepository;
+use App\Entity\Background;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MapController extends AbstractController
 {
+
+
     /**
      * @Route("/map", name="map")
      */
-    public function index()
+    public function index(BackgroundRepository $backgroundRepository)
     {
-
+        
+    	$citys = $backgroundRepository->findCity();
+    	dump($citys);
+    	#$httpClient = new \Http\Adapter\Guzzle6\Client();
+		#$provider = new \Geocoder\Provider\GoogleMaps\GoogleMaps($httpClient, null, 'AIzaSyAZowszml3xX0HWk1wZg1MOVEpb2Te_PVU');
+		#$geocoder = new \Geocoder\StatefulGeocoder($provider, 'en');
+		#$result = $geocoder->geocodeQuery(GeocodeQuery::create('Buckingham Palace, London'));
+		#$result = $geocoder->reverseQuery(ReverseQuery::fromCoordinates(...));
+		#dump($result);
+		#$test = json_encode($result);
 
 
         return $this->render('map/map2.html.twig', [
             'controller_name' => 'MapController',
+            'citys' => $citys
         ]);
     }
 }
--- src/Repository/BackgroundRepository.php
+++ src/Repository/BackgroundRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Background;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * @method Background|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +18,14 @@ class BackgroundRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Background::class);
+    }
+
+    public function findCity()
+    {
+        return $this->createQueryBuilder('b')
+        ->select('b.previous_city')
+        ->getQuery()
+        ->getResult();
     }
 
     // /**
