46483471b599b4e2f691f5ede469ecd713f6c965
Florian <florian.pabich@yahoo.fr>
2020-04-24 14:54:56 +0200

    logo

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -27,14 +27,14 @@ class RegistrationFormType extends AbstractType
                     new Email(['message' => 'Please enter a valid email;'])
                 ]
             ])
-            ->add('agreeTerms', CheckboxType::class, [
+            /*->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
-            ])
+            ])*/
             ->add('plainPassword', RepeatedType::class, [
    // instead of being set onto the object directly,
    // this is read and encoded in the controller
